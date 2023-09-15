e6bc3eaeabcb9a027f79d504fad774076accffdf
Petchiappan.P <Petchiappan.P@KTP-KIT008>
2023-09-15 17:59:59 +0530

    All are Finished

--- src/app/AuthGuard/auth.guard.ts
+++ src/app/AuthGuard/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { CanActivate, CanActivateFn, Router } from '@angular/router';
 import { LoginService } from '../Services/Login/login.service';
 
-
+//AuthGuards
 export const authGuard:CanActivateFn =(route,state) => {
   const currentmenu = route.url[0].path;
 const router = inject(Router);
